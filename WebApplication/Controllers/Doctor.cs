ccc77c66e44518833caf72b9fb581449de1af9b2
Faiz Alhallak <faizhallak@outlook.com>
2022-05-27 15:18:05 +0200

    Add project files.

---
+++ WebApplication/Controllers/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class Doctor : Controller
+    {
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+    }
+}
