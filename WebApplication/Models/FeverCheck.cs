29935e24825e46dfe7c7db028bdd16111bb07105
faizhallak <102051352+faizhallak@users.noreply.github.com>
2022-05-30 07:19:43 +0200

    Create FeverCheck.cs

---
+++ WebApplication/Models/FeverCheck.cs
@@ -0,0 +1,20 @@
+namespace WebApplication.Models
+{
+    public class FeverCheck
+    {
+        public static string WriteMessage()
+        {
+            return "Please enter your temperature: ";
+        }
+
+        public static string Check(int temp)
+        {
+            if (temp >= 38)
+                return "You have a fever.";
+            else if (temp <= 35)
+                return "You have Hypothermia";
+            else
+                return "You don't have a fever.";
+        }
+    }
+}
